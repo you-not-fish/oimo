46bc8bbb904b5d2d50ed1e0dcff8db80c37844c4
zifeiyu <1339734425@qq.com>
2024-02-03 02:22:42 +0000

    Create new project.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/cpp/oimo
+# Build directory: /root/cpp/oimo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
